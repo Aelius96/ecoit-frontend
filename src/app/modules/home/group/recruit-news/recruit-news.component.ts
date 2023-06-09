0abb81dfcc0c21967ef292dfd180e2599048f6cc
@vdb5421 <duongvb@ecoit.com.vn>
2023-06-09 12:08:17 +0700

    update
    abouthome: doi mau
    aside: 200%
    customer , new, recuit: chinh css

--- src/app/modules/home/group/recruit-news/recruit-news.component.ts
+++ src/app/modules/home/group/recruit-news/recruit-news.component.ts
@@ -3,6 +3,8 @@ import {News} from "../../../../core/model/news/news";
 import {Router} from "@angular/router";
 import {NewsService} from "../../../../services/news/news.service";
 import {TokenStorageService} from "../../../../services/token-storage/token-storage.service";
+import { Recruit } from 'src/app/core/model/recruit/recruit';
+import { RecruitService } from 'src/app/services/recruit/recruit.service';
 
 @Component({
   selector: 'app-recruit-news',
@@ -11,13 +13,16 @@ import {TokenStorageService} from "../../../../services/token-storage/token-stor
 })
 export class RecruitNewsComponent {
 
+  recruitList : Recruit [] = [];
   newsList: News[]=[];
   role: string;
   constructor(private router:Router, private newsService: NewsService,
-              private tokenStorageService: TokenStorageService) {}
+              private tokenStorageService: TokenStorageService ,
+              private recruitService:RecruitService,) {}
 
   ngOnInit(): void {
     this.getListAllWithPage();
+    this.getListAll_recuit();
 
   }
 
@@ -25,6 +30,12 @@ export class RecruitNewsComponent {
 
     this.newsService.listAll().subscribe(data=>
     {return this.newsList =data})
+  }
+
+  getListAll_recuit(){
+    this.recruitService.listAll().subscribe(data=>{
+      return this.recruitList = data;
+    })
   }
 
 
