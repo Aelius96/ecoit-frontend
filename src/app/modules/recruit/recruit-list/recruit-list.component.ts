bed9f78ac77b42299861c328dee857dbc63e08cc
@vdb5421 <duongvb@ecoit.com.vn>
2023-06-12 16:09:33 +0700

    css home , news-footer, recruit,

--- src/app/modules/recruit/recruit-list/recruit-list.component.ts
+++ src/app/modules/recruit/recruit-list/recruit-list.component.ts
@@ -1,42 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recruit } from '../../../core/model/recruit/recruit';
+import { RecruitService } from 'src/app/services/recruit/recruit.service';
 
 @Component({
   selector: 'app-recruit-list',
   templateUrl: './recruit-list.component.html',
   styleUrls: ['./recruit-list.component.css']
 })
-export class RecruitListComponent {
-  recruits=[
-    {
-      id: 1,
-      title: 'Specialized Design Tools for 2019',
-      excerpt: 'As a result, along with the larger corporate-backed applications, we have a huge host of small tools and …',
-      createdDate: 'January 14, 2019'
-    },
-    {
-      id: 2,
-      title: 'Specialized Design Tools for 2019',
-      excerpt: 'As a result, along with the larger corporate-backed applications, we have a huge host of small tools and …',
-      createdDate: 'January 14, 2019'
-    },
-    {
-      id: 3,
-      title: 'Specialized Design Tools for 2019',
-      excerpt: 'As a result, along with the larger corporate-backed applications, we have a huge host of small tools and …',
-      createdDate: 'January 14, 2019'
-    },
-    {
-      id: 4,
-      title: 'Specialized Design Tools for 2019',
-      excerpt: 'As a result, along with the larger corporate-backed applications, we have a huge host of small tools and …',
-      createdDate: 'January 14, 2019'
-    },
-    {
-      id: 5,
-      title: 'Specialized Design Tools for 2019',
-      excerpt: 'As a result, along with the larger corporate-backed applications, we have a huge host of small tools and …',
-      createdDate: 'January 14, 2019'
-    },
+export class RecruitListComponent implements OnInit {
+ 
+  recruitList: Recruit[]=[];
+  constructor(private recruitService:RecruitService) { }
 
-    ]
+
+
+  ngOnInit(): void {
+    this.getAllList()
+  }
+  getAllList():void{
+      this.recruitService.listAll().subscribe(data=>{
+        return this.recruitList=data
+      })
+  }
+
 }
