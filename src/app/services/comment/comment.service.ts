a03b6463a555559198a40a407873be73d87c8a26
Aelius96 <109273219+Aelius96@users.noreply.github.com>
2023-07-18 08:50:44 +0700

    list comment

--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Constant } from 'src/app/core/config/constant';
 import { Comment } from 'src/app/core/model/comment/comment';
+import {Domain} from "../../core/domain/domain";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ import { Comment } from 'src/app/core/model/comment/comment';
 export class CommentService {
 
   private baseUrl = `${Constant.BASE_URL}`;
+  private domain = `${Domain.COMMENT}`
+
   constructor( private http: HttpClient ) { }
 
   // getCommentChildById( params: any):Observable<any>{
@@ -17,23 +20,28 @@ export class CommentService {
   // }
 
   getCommentChildByParent():Observable<any>{
-    return this.http.get(`${this.baseUrl}/comment/get` )
+    return this.http.get(`${this.baseUrl}/${this.domain}/get` )
   }
+
+  getParentCommentActive():Observable<Comment[]>{
+    return this.http.get<Comment[]>(`${this.baseUrl}/${this.domain}/parent` )
+  }
+
   getListCommentHome():Observable<any>{
-    return this.http.get(`${this.baseUrl}/comment/home`);
+    return this.http.get(`${this.baseUrl}/${this.domain}/home`);
   }
   getListAll():Observable<Comment[]>{
-    return this.http.get<Comment[]>(`${this.baseUrl}/comment/all`)
+    return this.http.get<Comment[]>(`${this.baseUrl}/${this.domain}/all`)
   }
 
   DisableComment(id : number){
-    return this.http.get(`${this.baseUrl}/comment/disable/${id}`)
+    return this.http.get(`${this.baseUrl}/${this.domain}/disable/${id}`)
   }
   creatComment(creatcomment: any):Observable<Object>{
-    return this.http.post(`${this.baseUrl}/comment/create` , creatcomment)
+    return this.http.post(`${this.baseUrl}/${this.domain}/create` , creatcomment)
   }
   updateComment(id:number, updateComment: any):Observable<Object>{
-    return this.http.post(`${this.baseUrl}/comment/update/{id}` , updateComment)
+    return this.http.post(`${this.baseUrl}/${this.domain}/update/{id}` , updateComment)
   }
 
   getCommentByPostId(postId : number):Observable<any>{
