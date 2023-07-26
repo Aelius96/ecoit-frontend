771210aa42744435965608bb1f900557bd1604a2
@vdb5421 <duongvb@ecoit.com.vn>
2023-07-26 11:29:01 +0700

    update comment

--- src/app/_animation/animated-digit/animated-digit.component.ts
+++ src/app/_animation/animated-digit/animated-digit.component.ts
@@ -49,5 +49,31 @@ export class AnimatedDigitComponent implements AfterViewInit {
 
     step();
   }
+  
+//   Tham số đầu vào:
+
+// 'endValue:
+// element: any: Đây là tham chiếu đến phần tử HTML mà số đếm sẽ hiển thị trong đó. Tham chiếu này được đưa vào qua tham số element.
+// Các biến và hằng số:
+
+// steps = 10: S
+// durationMs: Thời gian tổng cộng để thực hiện hiệu ứng số đếm (tính bằng mili giây). Thời gian này được chọn ngẫu nhiên trong khoảng từ 3000 ms (3 giây) đến 4000 ms (4 giây).
+// stepCount: Số lần cần thực hiện để đạt đến endValue trong thời gian durationMs. Nó được tính bằng cách chia durationMs cho .steps
+// valueIncrement: Bư
+// sinValueIncrement: Bước tăng giá trị của hàm số sine cho mỗi lần chuyển động. Nó được tính bằng cách lấy giá trị pi (π) chia cho stepCount.
+// Bắt đầu hiệu ứng số đếm:
+
+// 'currentValue =
+// currentSinValue = 0: Đây là giá trị hiện tại của hàm số sine trong số đếm. Ban đầu, nó được thiết lập thành 0.
+// Hàm step():
+
+// Đây là hàm đệ quy được sử dụng để thực hiện mỗi bước chuyển động trong số đếm.
+// Trong mỗi lần gọi step(), currentSinValue sẽ tăng lên một bước sinValueIncrement, và currentValue sẽ tăng lên valueIncrement nhưng được điều chỉnh bởi giá trị của hàm số sine.
+// Giá trị currentValue sau đó được hiển thị trong phần tử HTML được định nghĩa bởi element.
+// Khi currentSinValue chưa đạt đến giá trị pi (π) (khoảng 3.14159), hàm step() được gọi lại thông qua window.requestAnimationFrame(step);, tiếp tục thực hiện các bước chuyển động cho đến khi số đếm đạt đến endValue.
+// Khi số đếm đạt đến endValue, hàm kiểm tra nếu giá trị đạt được có bằng this.digit (giá trị tham chiếu được sử dụng để kiểm tra điều kiện hiển thị dấu "+"). Nếu điều kiện đúng, dấu "+" sẽ được thêm vào phần tử hiển thị.
+// Lưu ý: Đoạn mã trên sử dụng hàm số sine để tạo hiệu ứng mượt mà trong số đếm. Hiệu ứng này có thể tạo ra một số giá trị trung gian nhỏ khi số đếm đang chuyển động. Nếu bạn muốn hiệu ứng số đếm thực hiện theo các bước tăng cố định, bạn có thể xem xét sử dụng một phương pháp số học đơn giản hơn.
+
+
 
 }
